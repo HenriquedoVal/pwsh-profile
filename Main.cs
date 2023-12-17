cfe3a8459d62bb32775f1b41e6b90435f6529e2c
Henrique <henrique.val@hotmail.com>
2023-12-17 01:40:28 -0300

    check %path% before expanding it

--- Main.cs
+++ Main.cs
@@ -6,84 +6,100 @@ namespace MyConfig
     public class Init : IModuleAssemblyInitializer
     {
         public void OnImport()
-		{
-			Type ReadLine = null;
-			Type SetPSReadLineOption = null;
+        {
+            Type PSConsoleReadLine = null;
+            Type SetPSReadLineOption = null;
 
-			var Asm = AppDomain.CurrentDomain.Load(
-				"Microsoft.PowerShell.PSReadLine2"
-			);
+            var Asm = AppDomain.CurrentDomain.Load(
+                "Microsoft.PowerShell.PSReadLine2"
+            );
 
-			foreach (Type t in Asm.GetExportedTypes())
-			{
-				if (t.Name == "PSConsoleReadLine") ReadLine = t;
-				if (t.Name == "SetPSReadLineOption") SetPSReadLineOption = t;
-			}
+            PSConsoleReadLine = Asm.GetType(
+                    "Microsoft.PowerShell.PSConsoleReadLine"
+            );
+            SetPSReadLineOption = Asm.GetType(
+                    "Microsoft.PowerShell.SetPSReadLineOption"
+            );
 
-			if (
-					ReadLine is null
-					|| SetPSReadLineOption is null
-			)
-				throw new Exception("Types not found");
+            if (
+                    PSConsoleReadLine is null
+                    || SetPSReadLineOption is null
+            )
+                throw new Exception("Types not found");
 
-			var inst = Activator.CreateInstance(SetPSReadLineOption);
-			
-			const int Vi = 2;
-			var Prop = SetPSReadLineOption.GetProperty("EditMode");
-			Prop.SetValue(inst, Vi);
+            var inst = Activator.CreateInstance(SetPSReadLineOption);
+            
+            const int Vi = 2;
+            var Prop = SetPSReadLineOption.GetProperty("EditMode");
+            Prop.SetValue(inst, Vi);
 
-			const int ListView = 1;
-			Prop = SetPSReadLineOption.GetProperty("PredictionViewStyle");
-			Prop.SetValue(inst, ListView);
+            const int ListView = 1;
+            Prop = SetPSReadLineOption.GetProperty("PredictionViewStyle");
+            Prop.SetValue(inst, ListView);
 
-			const int script = 3;
+            Prop = SetPSReadLineOption.GetProperty("MaximumHistoryCount");
+            Prop.SetValue(inst, 12288);
+
+            const int script = 3;
             Prop = SetPSReadLineOption.GetProperty("ViModeIndicator");
             Prop.SetValue(inst, script);
 
-			var modeChange = ScriptBlock.Create(
-				@"if ($args[0] -eq 'Command') {
+            var modeChange = ScriptBlock.Create(
+                @"if ($args[0] -eq 'Command') {
     Write-Host -NoNewLine ""`e[1 q""
 } else {
     Write-Host -NoNewLine ""`e[5 q""
 }"
-			);
+            );
             Prop = SetPSReadLineOption.GetProperty("ViModeChangeHandler");
             Prop.SetValue(inst, modeChange);
-			
-			Prop = SetPSReadLineOption.GetProperty("ContinuationPrompt");
-			Prop.SetValue(inst, "> ");
+            
+            Prop = SetPSReadLineOption.GetProperty("ContinuationPrompt");
+            Prop.SetValue(inst, "> ");
 
-			var SetOptions = ReadLine.GetMethod("SetOptions");
+            var SetOptions = PSConsoleReadLine.GetMethod("SetOptions");
 
-			SetOptions.Invoke(null, new object[] {inst});
+            SetOptions.Invoke(null, new object[] {inst});
 
-			// Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-			// Set-Alias -Name hist -Value Search-ShellServerHistory
-			// Set-Alias -Name swopt -Value Switch-ShellServerOptions
-			// Set-Alias -Name swtheme -Value Switch-ShellServerTheme
+            // Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+            // Set-Alias -Name hist -Value Search-ShellServerHistory
+            // Set-Alias -Name swopt -Value Switch-ShellServerOptions
+            // Set-Alias -Name swtheme -Value Switch-ShellServerTheme
 
-			var pwsh = PowerShell.Create(RunspaceMode.CurrentRunspace);
-			pwsh.AddCommand("Set-PSReadlineKeyHandler")
-				.AddParameter("Key", "Tab")
-				.AddParameter("Function", "MenuComplete")
-				.AddStatement()
+            var pwsh = PowerShell.Create(RunspaceMode.CurrentRunspace);
+            pwsh.AddCommand("Set-PSReadlineKeyHandler")
+                .AddParameter("Key", "Tab")
+                .AddParameter("Function", "MenuComplete")
+                .AddStatement()
 
-				.AddCommand("Set-Alias")
-				.AddParameter("Name", "hist")
-				.AddParameter("Value", "Search-ShellServerHistory")
-				.AddStatement()
+                .AddCommand("Set-Alias")
+                .AddParameter("Name", "hist")
+                .AddParameter("Value", "Search-ShellServerHistory")
+                .AddStatement()
 
-				.AddCommand("Set-Alias")
-				.AddParameter("Name", "swopt")
-				.AddParameter("Value", "Switch-ShellServerOptions")
-				.AddStatement()
+                .AddCommand("Set-Alias")
+                .AddParameter("Name", "swopt")
+                .AddParameter("Value", "Switch-ShellServerOptions")
+                .AddStatement()
 
-				.AddCommand("Set-Alias")
-				.AddParameter("Name", "swtheme")
-				.AddParameter("Value", "Switch-ShellServerTheme")
-				.Invoke();
+                .AddCommand("Set-Alias")
+                .AddParameter("Name", "swtheme")
+                .AddParameter("Value", "Switch-ShellServerTheme")
+                .Invoke();
 
-			pwsh.Dispose();
+            pwsh.Dispose();
+
+            string current_path = Environment.GetEnvironmentVariable("path");
+            if (current_path is null) return;
+
+            string to_add = Environment.ExpandEnvironmentVariables("%userprofile%\\ps_scripts");
+            foreach (string path in current_path.Split(';'))
+            {
+                if (path == to_add) return;
+            }
+
+            string semi = current_path[current_path.Length - 1] == ';' ? "" : ";";
+            Environment.SetEnvironmentVariable("path", $"{current_path}{semi}{to_add}");
         }
     }
 }
